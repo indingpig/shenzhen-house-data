e110a19f189479f71e12ee70cf4211650564d06f
indingpig <indoing@gmal.com>
2025-03-21 11:30:02 +0800

    完成获取bing的每日一图

--- backend/app/routes/auth/auth.py
+++ backend/app/routes/auth/auth.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, current_app
+from datetime import datetime, timedelta
+import requests
 from backend.app.config import API_PREFIX
 from backend.app.services.create_captcha import captcha_image
 from backend.app.utils.response import BaseResource
@@ -26,12 +28,35 @@ def login():
     print(username, password, captcha, uuid)
     # 校验验证码
     if not captcha or not uuid:
-        return BaseResource.error(message="请先获取验证码")
+        return BaseResource.success(message="请先获取验证码",code=1100)
     correct_answer = redis_helper.get_captcha(uuid)
     redis_helper.delete(uuid)
     if not correct_answer:
-        return BaseResource.error(message="验证码已过期，请重新获取")
+        return BaseResource.success(message="验证码已过期，请重新获取", code=1100)
     if captcha.lower() != correct_answer:
-        return BaseResource.error(message="验证码错误")
+        return BaseResource.success(message="验证码错误", code=1100)
 
     return BaseResource.success(data={"token": "fake_token"})
+
+@auth_bp.route("/get_bing_picture", methods=["GET"])
+def get_bing_picture():
+    day = request.args.get("day")
+    if not day:
+        return BaseResource.error(message="ssd 不能为空", code=1100)
+    day_now = datetime.now() - timedelta(days=int(day))
+    ssd = day_now.strftime('%Y%m%d') + '_0700'
+    """获取必应每日一图"""
+    url = f'https://cn.bing.com/hp/api/v1/imagegallery?format=json&ssd={ssd}_0700&FORM=BEHPTB&ensearch=1'
+    response = requests.get(url)
+    if response.status_code != 200:
+        return BaseResource.error(message="获取图片失败", code=1100)
+    try:
+        data = response.json()
+        images_list = data.get('data').get('images')
+        image = images_list[int(day)].get('imageUrls').get('landscape').get('ultraHighDef')
+        return BaseResource.success(data={"url": image})
+    except Exception as e:
+        current_app.logger.error(
+            f"API: {request.path}, 方法: {get_bing_picture.__name__}, 错误: {e}",
+        )
+        return BaseResource.error(message="获取图片失败", code=1100)
